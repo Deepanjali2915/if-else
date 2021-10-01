1a565a311a89ef06462905bfeafcab365d548d8e
Deepanjali <89567807+Deepanjali2915@users.noreply.github.com>
2021-10-01 14:11:33 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,20 +1,18 @@
-# a=True+False
+
+
+
+# a="deepu"
+# a+="anjali"
 # print(a)
 
-# a=True
-# b=12
-# print(a+b)
-
-# a="false"
-# b=13
-# print(a+b)
-
-
-# a=True
-# b="false"
-# print(a+b)
+# i=0
+# while i<10:
+#     i=i+1
+#     if i==3:
+#         continue
+#     if i==9:
+#         continue
+#     print(i)
 
 
-# a="true"
-# b=False
-# print("true134True")
+
